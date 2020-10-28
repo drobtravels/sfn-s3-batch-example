965d71f78c631538bdbd915cf07b8dd8ea097083
David Roberts <djrob@amazon.com>
2020-10-28 16:47:22 -0400

    initial setup of bucket with inventory

--- lib/big-fan-out-stack.ts
+++ lib/big-fan-out-stack.ts
@@ -1,9 +1,25 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as s3 from '@aws-cdk/aws-s3'
 
 export class BigFanOutStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const inventoryBucket = new s3.Bucket(this, 'InventoryBucket');
+
+    const dataBucket = new s3.Bucket(this, 'DataBucket', {
+      inventories: [
+        {
+          frequency: s3.InventoryFrequency.DAILY,
+          includeObjectVersions: s3.InventoryObjectVersion.CURRENT,
+          destination: {
+            bucket: inventoryBucket,
+          },
+          format: s3.InventoryFormat.PARQUET
+        }
+      ]
+    })
+
+
   }
 }
